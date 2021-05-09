f4c8e7a94ab697e78243e5eed1d67fe788973cf8
Federico 'Magus' Rocca <frocca17@gmail.com>
2021-05-09 17:48:14 -0300

    Day 2 done

---
+++ Day 2/Classes/PasswordPolicies.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2.Classes
+{
+    class PasswordPolicies
+    {
+        public int minOcurrencies { get; set; }
+        public int maxOcurrencies { get; set; }
+        public char character { get; set; }
+        public String password { get; set; }
+    }
+}
